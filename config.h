8a8d1e5f621586305750010b200fa2d098aab345
rs288 <davids2804@pm.me>
2021-02-18 20:41:52 -0600

    add move with hjkl

--- config.h
+++ config.h
@@ -62,7 +62,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 
-
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -71,6 +70,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      spawn,          SHCMD("herramientas") },
 	{ MODKEY,  	                XK_p,      spawn,          SHCMD("power") },
 	{ MODKEY|ShiftMask,             XK_k,      spawn,          SHCMD("keymaps") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("letras") },
+	{ MODKEY,                       XK_q,      spawn,          SHCMD("flechas") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
